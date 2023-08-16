465f6a8e6024e1b8d6a49da503c9d460b12b0de6
Ganesh99121 <125423631+Ganesh99121@users.noreply.github.com>
2023-08-16 10:49:07 +0530

    Update mongoose.js

--- Hospital-management-nodejs-master/server/db/mongoose.js
+++ Hospital-management-nodejs-master/server/db/mongoose.js
@@ -2,14 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-// Change the database connection URL with yours
-mongoose.connect('mongodb+srv://ganeshmmarolix:GHWKf3Amlwl822fr@cluster0.ykazdov.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB successfully!');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error.message);
-});
+// Change the database with yours
+mongoose.connect(
+  "mongodb+srv://ganeshmmarolix:GHWKf3Amlwl822fr@cluster0.ykazdov.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true } // Added options
+);
 
 module.exports = { mongoose };
