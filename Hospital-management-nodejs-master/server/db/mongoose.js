b725137a718153cf8f1f5e88729d5378125ede25
Ganesh99121 <125423631+Ganesh99121@users.noreply.github.com>
2023-08-16 10:51:32 +0530

    Update mongoose.js

--- Hospital-management-nodejs-master/server/db/mongoose.js
+++ Hospital-management-nodejs-master/server/db/mongoose.js
@@ -3,9 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Change the database with yours
-mongoose.connect(
-  "mongodb+srv://ganeshmmarolix:GHWKf3Amlwl822fr@cluster0.ykazdov.mongodb.net/test?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true } // Added options
-);
-
+mongoose.connect("mongodb+srv://ganeshmmarolix:GHWKf3Amlwl822fr@cluster0.ykazdov.mongodb.net/test?retryWrites=true&w=majority",
+{ useNewUrlParser: true, useUnifiedTopology: true } // Added options);
 module.exports = { mongoose };
